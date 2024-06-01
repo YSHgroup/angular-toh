9bdf2043b5a9c0a9b4d170cacefab68d376849e5
YSHgroup <david.dev100@outlook.com>
2024-06-01 10:28:23 -0400

    Add input decorator to bind data to child component and style

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
-  standalone: true,
-  imports: [],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.scss'
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
