c9df79be04b120e96f7c92d60c656ebced348b6f
YSHgroup <david.dev100@outlook.com>
2024-05-31 20:58:04 -0400

    Make app module and ngModel for two-way-data binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
-  standalone: true,
-  imports: [],
+  // standalone: true,
+  // imports: [CommonModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss'
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 }
+
