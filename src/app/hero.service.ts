c5500baf192b2256de384de62a2a391ba13cf7e7
YSHgroup <david.dev100@outlook.com>
2024-06-06 21:36:50 -0400

    Make service available to use HTTP client server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -10,12 +11,19 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    // const heroes = of(HEROES);
+    // this.messageService.add('HeroService: fetched heroes');
+    // return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
